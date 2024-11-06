f3c61962d6ba1360a24f6da67d324e3c4a13330f
ImpanaD142 <2022cs_impanad_b@nie.ac.in>
2024-11-06 09:54:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NewsModule from './Components/News';
+
+const News = () => {
+  return (
+    <div className='container'>
+      <h1>News World</h1>
+      <NewsModule />
+    </div>
+  );
+};
+
+export default News;
+
+
